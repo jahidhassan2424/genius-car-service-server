36e00cf6739e917815afcc77dbd89c84d798ae8a
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-04-25 18:01:08 +0600

    Created genius car server

---
+++ _steps.js
@@ -0,0 +1,68 @@
+/**
+ * 
+ * /**
+ * 1. make a folder and open cmd in that folder
+ * 2. npm init -y
+ * 3. npm install express
+ * ----------------------------
+ * For Express
+ * ---------------------------- 
+ * 1. require express
+ * 2. Create app variable
+ * 3. Declear port
+ * 4. set app.get('/')
+ * 5. listen to port
+ * 6. node index.js
+ * 7. check your browser for that port
+ * 8. npm i nodemon
+ * 9. add "start":"node index.js", in package.json script{}
+ * 9. add "start-dev":"nodemon index.js", in package.json script{}
+ * 
+
+ * 
+ * --------------------------------------------------------
+ * Integrate Sending Data from client to server
+ * --------------------------------------------------------
+ * 1. Create a form in client side to add user.
+ * 2. onSubmit get form data
+ * 3. ON SERVER: Create user post method to receive data on backend
+ * 4. On client side: set fetch with post,headers,body
+ * 5. Make sure you return a json form in the POST API or else client side cannot parse string into json.
+ * 
+ * --------------------------------------------------------
+ * Load data from database to client side
+ * --------------------------------------------------------
+ * 1.   Create get API
+ * 2. create a query object
+ * 3. collection.find(query)
+ * 4. cursor.toArray()
+ * 5. return the result
+ * 6. from client useEffect and display like you have done before
+ * 
+ * 
+ * ----------------------------------------------------
+ * DELETE
+ * ----------------------------------------------------
+ * 1.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+
+
+
+
